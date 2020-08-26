c6efded1550e4091500617ff7676beeb75bc4fb8
AK Hanish <akhanish7@gmail.com>
2020-08-26 05:39:53 +0530

    Form working and logging

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(public http: HttpClient) {}
 
-  constructor() {}
-
+  log(result: any) {
+    console.log(result);
+  }
 }
